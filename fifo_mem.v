67b9b51ce8e086fe100625f2e15db442ef159422
Bmk10 <56593623+MANISHBMK10@users.noreply.github.com>
2024-06-17 20:43:21 -0400

    fif0_async

---
+++ fifo_mem.v
@@ -0,0 +1,22 @@
+module fifomem #(parameter DATASIZE = 8, // Memory data word width
+                 parameter ADDRSIZE = 4) // Number of mem address bits
+    (output reg [DATASIZE-1:0] rdata,
+     input [DATASIZE-1:0] wdata,
+     input [ADDRSIZE-1:0] waddr, raddr,
+     input wclken, wfull, wclk,rclk, rempty, rclken);
+    
+ // RTL Verilog memory model
+     localparam DEPTH = 1<<ADDRSIZE;
+     reg [DATASIZE-1:0] mem [0:DEPTH-1];
+    reg rdata_val;
+   //assign rdata = rdata_val;
+    
+	always @(posedge wclk)
+         if (wclken && !wfull)
+     		 mem[waddr] <= wdata;
+
+     always @(posedge rclk)
+      if(rclken && !rempty)
+        rdata <= mem[raddr]; 
+ 
+endmodule
---
+++ wfull.v
@@ -0,0 +1,29 @@
+module wptr_full #(parameter ADDRSIZE = 4)
+   (output reg wfull,
+    output [ADDRSIZE-1:0] waddr,
+    output reg [ADDRSIZE :0] wptr,
+    input [ADDRSIZE :0] wq2_rptr,
+    input winc, wclk, wrst_n);
+
+   reg [ADDRSIZE:0] wbin;
+   wire [ADDRSIZE:0] wgraynext, wbinnext,wfull_val;
+
+   always @(posedge wclk or negedge wrst_n)
+     if (!wrst_n) 
+       {wbin, wptr} <= 0;
+     else
+       {wbin, wptr} <= {wbinnext, wgraynext};
+
+   assign waddr = wbin[ADDRSIZE-1:0];
+   assign wbinnext = wbin + (winc & ~wfull);
+   assign wgraynext = (wbinnext>>1) ^ wbinnext;
+  
+    assign wfull_val = (wgraynext=={~wq2_rptr[ADDRSIZE:ADDRSIZE-1],
+				    wq2_rptr[ADDRSIZE-2:0]});
+   
+   always @(posedge wclk or negedge wrst_n)
+     if (!wrst_n)
+       wfull <= 1'b0;
+     else
+       wfull <= wfull_val;
+endmodule
