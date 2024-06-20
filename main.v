a13a722aa12217c58ea9d7e24e8fdbfc03812e62
Bmk10 <56593623+MANISHBMK10@users.noreply.github.com>
2024-06-20 10:13:01 -0400

    updated fifo

--- main.v
+++ main.v
@@ -1,33 +1,12 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 14.03.2024 12:42:51
-// Design Name: 
-// Module Name: memory_loader
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module memory_loader (
     output reg [7:0] port_O,
     input clk,
+    input clk2,
     input rst,
     input signed [7:0] port_A,     // Input port for feature matrix data
     input signed [7:0] port_W,     // Input port for weight matrix data
     input write_enable_A,  // Control signal for writing to Feature Memory
-    input write_enable_W,   // Control signal for writing to Weight Memory
+    input write_enable_W,  // Control signal for writing to Weight Memory
     input startSignal
 );
 
@@ -35,7 +14,7 @@ module memory_loader (
 reg signed [7:0] Feature_Memory [0:31];  // 8-bit wide : 16 elements of Feature Memory Input and the next 16 elments for Output of Matrix Multiplication, 
 reg signed [7:0] Weight_Memory [0:15];   // 16 elements, 8-bit wide
 reg ReadEnable;
-wire [0:4] count;
+wire [4:0] count;
 
 reg Wen;
 reg [7:0] inp_west0, inp_west1, inp_west2, inp_west3, inp_north0, inp_north1, inp_north2, inp_north3;
@@ -46,7 +25,36 @@ integer i;  // Feature Memory index
 integer j;  // Weight Memory index
 integer k;  // Reading output from Feature Memory
 
-/////////////////////// Loading memoty
+// FIFOs
+wire [7:0] fifo_data_out_A, fifo_data_out_W;
+wire fifo_full_A, fifo_empty_A, fifo_full_W, fifo_empty_W;
+reg fifo_write_en_A, fifo_write_en_W, fifo_read_en_A, fifo_read_en_W;
+
+fifo1 #(8, 4) feature_fifo (
+    .wdata(Feature_Memory[i]),
+    .winc(fifo_write_en_A),
+    .wclk(clk2),
+    .wrst_n(~rst),
+    .rinc(fifo_read_en_A),
+    .rclk(clk2),
+    .rrst_n(~rst),
+    .rdata(fifo_data_out_A),
+    .wfull(fifo_full_A),
+    .rempty(fifo_empty_A)
+);
+
+fifo1 #(8, 4) weight_fifo (
+    .wdata(Weight_Memory[j]),
+    .winc(fifo_write_en_W),
+    .wclk(clk2),
+    .wrst_n(~rst),
+    .rinc(fifo_read_en_W),
+    .rclk(clk2),
+    .rrst_n(~rst),
+    .rdata(fifo_data_out_W),
+    .wfull(fifo_full_W),
+    .rempty(fifo_empty_W)
+);
 
 // Writing Logic (Feature Memory)
 always @(posedge clk) begin
@@ -68,149 +76,135 @@ always @(posedge clk) begin
     end
 end
 
+// Transfer data from memory to FIFO
+always @(posedge clk2) begin
+    if (rst) begin
+        fifo_write_en_A <= 0;
+        fifo_write_en_W <= 0;
+    end else if (startSignal) begin
+        if (i > 0 && !fifo_full_A) begin
+            fifo_write_en_A <= 1;
+        end else begin
+            fifo_write_en_A <= 0;
+        end
+        
+        if (j > 0 && !fifo_full_W) begin
+            fifo_write_en_W <= 1;
+        end else begin
+            fifo_write_en_W <= 0;
+        end
+    end
+end
 
-/////////////////////// Start Matrix multiplication
-//always@(posedge clk)
-//begin
-
+// Systolic Array Instantiation
 Systolic_Array SA_1(count, Activation_result12, Activation_result13, Activation_result14, Activation_result15,inp_north0, inp_north1, inp_north2, inp_north3, inp_west0, inp_west1, inp_west2, inp_west3,Wen, clk, rst,startSignal);
-//end
-
 initial begin 
     #69
-    if (startSignal==1) //WEn
-    begin
-    #5 Wen=1;
-
-//	#20 Wen =1;
-
-//	#20 Wen=1;
-
-//	#20 Wen =1;
-
-	#100 Wen=0;
-
+    if (startSignal == 1) begin
+        #5 Wen = 1;
+        #100 Wen = 0;
     end
-
 end
-reg [4:0]cnt;
-always@(posedge clk)begin
-    if(rst)
+
+reg [4:0] cnt;
+always @(posedge clk2) begin
+    if (rst)
         cnt <= 4'd0;
-    else if (startSignal)begin
-        cnt <= cnt +1;
-    end
+    else if (startSignal)
+        cnt <= cnt + 1;
 end
 
-always@(*) begin
-    if(startSignal && cnt <=3)begin
+always @(*) begin
+    if (startSignal && cnt <= 3) begin
         Wen <= 1;
-        inp_north0 <= Weight_Memory[12-(cnt*4)];
-        inp_north1 <= Weight_Memory[13-(cnt*4)];
-        inp_north2 <= Weight_Memory[14-(cnt*4)];
-        inp_north3 <= Weight_Memory[15-(cnt*4)];
-    end
-    else
-        Wen<= 0;
-end
-always @(*)begin
-    if(startSignal && cnt >3 && cnt <=7)begin
-        inp_west0 <= Feature_Memory[(cnt-4)*4];
+        inp_north0 <= fifo_data_out_W;
+        inp_north1 <= fifo_data_out_W;
+        inp_north2 <= fifo_data_out_W;
+        inp_north3 <= fifo_data_out_W;
+        fifo_read_en_W <= 1;
+    end else begin
+        Wen <= 0;
+        fifo_read_en_W <= 0;
     end
 end
 
-
-always @(*)begin
-    if(startSignal && cnt >4 && cnt <=8)begin
-        inp_west1 <= Feature_Memory[(cnt-5)*4 +1];    
+always @(*) begin
+    if (startSignal && cnt > 3 && cnt <= 7) begin
+        inp_west0 <= fifo_data_out_A;
+        fifo_read_en_A <= 1;
+    end else begin
+        fifo_read_en_A <= 0;
     end
 end
 
-always @(*)begin
-    if(startSignal && cnt >5 && cnt <=9)begin
-        inp_west2 <= Feature_Memory[(cnt-6)*4+2];  
+always @(*) begin
+    if (startSignal && cnt > 4 && cnt <= 8) begin
+        inp_west1 <= fifo_data_out_A;
+        fifo_read_en_A <= 1;
+    end else begin
+        fifo_read_en_A <= 0;
     end
 end
 
-always @(*)begin
-    if(startSignal && cnt >6 && cnt <=10)begin
-        inp_west3 <= Feature_Memory[(cnt-7)*4+3];
+always @(*) begin
+    if (startSignal && cnt > 5 && cnt <= 9) begin
+        inp_west2 <= fifo_data_out_A;
+        fifo_read_en_A <= 1;
+    end else begin
+        fifo_read_en_A <= 0;
     end
 end
 
-
-
-
-
-
-always@(posedge clk)
-begin
-if(count ==  11) // check to use always block
-
-begin
-         Feature_Memory[16] <= Activation_result12;  // First Output
-
+always @(*) begin
+    if (startSignal && cnt > 6 && cnt <= 10) begin
+        inp_west3 <= fifo_data_out_A;
+        fifo_read_en_A <= 1;
+    end else begin
+        fifo_read_en_A <= 0;
+    end
 end
 
-else if(count==12)
-begin
-         Feature_Memory[20] <= Activation_result12;
-         Feature_Memory[17] <= Activation_result13;
-end
-
-else if(count==13)
-begin
+always @(posedge clk2) begin
+    if (count == 11) begin
+        Feature_Memory[16] <= Activation_result12;  // First Output
+    end else if (count == 12) begin
+        Feature_Memory[20] <= Activation_result12;
+        Feature_Memory[17] <= Activation_result13;
+    end else if (count == 13) begin
         Feature_Memory[24] <= Activation_result12;
-        Feature_Memory[21] = Activation_result13;
-        Feature_Memory[18]= Activation_result14;
-end
-    
-    else if (count ==14)
-    begin
-          Feature_Memory[28] <= Activation_result12;
-          Feature_Memory[25] = Activation_result13;
-          Feature_Memory[22] = Activation_result14;
-          Feature_Memory[19] = Activation_result15;
+        Feature_Memory[21] <= Activation_result13;
+        Feature_Memory[18] <= Activation_result14;
+    end else if (count == 14) begin
+        Feature_Memory[28] <= Activation_result12;
+        Feature_Memory[25] <= Activation_result13;
+        Feature_Memory[22] <= Activation_result14;
+        Feature_Memory[19] <= Activation_result15;
+    end else if (count == 15) begin
+        Feature_Memory[29] <= Activation_result13;
+        Feature_Memory[26] <= Activation_result14;
+        Feature_Memory[23] <= Activation_result15;
+    end else if (count == 16) begin
+        Feature_Memory[30] <= Activation_result14;
+        Feature_Memory[27] <= Activation_result15; 
+    end else if (count == 17) begin
+        Feature_Memory[31] <= Activation_result15;
+        ReadEnable <= 1;
     end
-
-    else if(count==15)
-    begin
-
-          Feature_Memory[29]= Activation_result13;
-          Feature_Memory[26] = Activation_result14;
-          Feature_Memory[23] = Activation_result15;
-    end
-
-    else if (count==16)
-    begin
-
-          Feature_Memory[30]=Activation_result14;
-          Feature_Memory[27]= Activation_result15; 
-    end
-
-    else if (count==17)
-    begin
-        Feature_Memory[31]= Activation_result15;
-       ReadEnable <= 1;
-    end
-    
-
 end
 
-always @(posedge clk) begin
+always @(posedge clk2) begin
     if (rst) begin
         k <= 16;  // Reset index
-    end else if (ReadEnable && (k<32)) begin
-         port_O <= Feature_Memory[k];
+    end else if (ReadEnable && (k < 32)) begin
+        port_O <= Feature_Memory[k];
         k <= k + 1;
     end
 end
-        
-    
+
 endmodule
 
 
-
-
-
-
+//C:\iverilog\bin>iverilog -o a FIFO_TPU_TB.v FIFO_TPU.v async_fifo.v systolicArray.v MACUnit.v QuantizationUnit.v ActivationUnit.v
+// iverilog -o a FIFO_TPU_TB.v FIFO_TPU.v async_fifo.v systolicArray.v MACUnit.v QuantizationUnit.v ActivationUnit.v
+//iverilog -o a FIFO_TPU.v systolicArray.v MACUnit.v QuantizationUnit.v ActivationUnit.v top_fifo.v sync_r2w.v sync_w2r.v rempty.v fifo_mem.v wfull.v
+// iverilog -o a FIFO_TPU_TB.v FIFO_TPU.v systolicArray.v MACUnit.v QuantizationUnit.v ActivationUnit.v top_fifo.v sync_r2w.v sync_w2r.v rempty.v fifo_mem.v wfull.v
--- mainTB.v
+++ mainTB.v
@@ -9,6 +9,7 @@ module memory_loader_tb;
     reg write_enable_A;        // Control signal for writing to Feature Memory
     reg write_enable_W;        // Control signal for writing to Weight Memory
      wire [7:0] port_O;
+    reg clk2; 
    // reg [7:0] Feature_Memory [0:15];  // 16 elements, 8-bit wide
     //reg [7:0] Weight_Memory [0:15];
     // Instantiate the memory_loader module
@@ -20,7 +21,8 @@ module memory_loader_tb;
         .port_W(port_W),
         .write_enable_A(write_enable_A),
         .write_enable_W(write_enable_W),
-        .startSignal(startSignal)
+        .startSignal(startSignal),
+        .clk2(clk2)
       
         
     );
@@ -29,12 +31,14 @@ module memory_loader_tb;
     integer k;
 
     // Clock generation
+   
+    // Clock generation for clk
     initial clk = 0;
-    //always #10 clk = ~clk; // Clock with a period of 10 ns for 100 MHz
-    initial begin
-	repeat(250) // Old Value 33
-		#10 clk <= ~clk; 
-end  
+    always #10 clk = ~clk; // Clock with a period of 10 ns (100 MHz)
+
+    // Clock generation for clk2
+    initial clk2 = 0;
+    always #10 clk2 = ~clk2; // Clock with a period of 20 ns (50 MHz)
     // Testbench logic
     initial begin
         // Initialize signals
@@ -45,61 +49,61 @@ end
         write_enable_W = 0;
 
         // Release reset and start initialization after two clock cycles
-        #40;
-        rst = 0;
+        #20;
+       rst = 0;
         startSignal=0;
-                          /// ***** Provide Your Inputs Below
+                          
 
         // Load the weight memory
         write_enable_W = 1;  // Enable writing to weight memory
-        port_W = 8'd4; #20;
-        port_W = 8'd0; #20;
-        port_W = 8'd2; #20;
-        port_W = 8'd1; #20;
+        port_W = 8'd4; #10;
+        port_W = 8'd0; #10;
+        port_W = 8'd2; #10;
+        port_W = 8'd1; #10;
         
-        port_W = 8'd4; #20;
-        port_W = 8'd3; #20;
-        port_W = 8'd2; #20;
-        port_W = 8'd0; #20;
+        port_W = 8'd4; #10;
+        port_W = 8'd3; #10;
+        port_W = 8'd2; #10;
+        port_W = 8'd0; #10;
         
-        port_W = 8'd4; #20;
-        port_W = 8'd3; #20;
-        port_W = 8'd0; #20;
-        port_W = 8'd1; #20;
+        port_W = 8'd4; #10;
+        port_W = 8'd3; #10;
+        port_W = 8'd0; #10;
+        port_W = 8'd1; #10;
         
-        port_W = 8'd4; #20;
-        port_W = 8'd3; #20;
-        port_W = 8'd2; #20;
-        port_W = 8'd1; #20;
+        port_W = 8'd4; #10;
+        port_W = 8'd3; #10;
+        port_W = 8'd2; #10;
+        port_W = 8'd1; #10;
         write_enable_W = 0;  // Disable writing to weight memory
 
         // Load the feature memory
-        write_enable_A = 1;  // Enable writing to feature memory
-        port_A = 8'd1; #20;
-        port_A = 8'd2; #20;
-        port_A = 8'd3; #20;
-        port_A = 8'd4; #20;
+       write_enable_A = 1;  // Enable writing to feature memory
+        port_A = 8'd1; #10;
+        port_A = 8'd2; #10;
+        port_A = 8'd3; #10;
+        port_A = 8'd4; #10;
         
-        port_A = 8'd1; #20;
-        port_A = 8'd2; #20;
-        port_A = 8'd3; #20;
-        port_A = 8'd4; #20;
+        port_A = 8'd1; #10;
+        port_A = 8'd2; #10;
+        port_A = 8'd3; #10;
+        port_A = 8'd4; #10;
         
-        port_A = 8'd1; #20;
-        port_A = 8'd2; #20;
-        port_A = 8'd3; #20;
-        port_A = 8'd4; #20;
+        port_A = 8'd1; #10;
+        port_A = 8'd2; #10;
+        port_A = 8'd3; #10;
+        port_A = 8'd4; #10;
         
-        port_A = 8'd1; #20;
-        port_A = 8'd2; #20;
-        port_A = 8'd3; #20;
-        port_A = 8'd4; #20;
+        port_A = 8'd1; #10;
+        port_A = 8'd2; #10;
+        port_A = 8'd3; #10;
+        port_A = 8'd4; #10;
         write_enable_A = 0; 
         startSignal =1; 
       
         // Display the values loaded into Feature Memory
         
-        $display("Feature Memory:");
+       $display("Feature Memory:");
         for (i = 0; i < 16; i = i + 1) begin
             $display("[%0d] = %d \t ", i, uut.Feature_Memory[i]);
         end
@@ -118,22 +122,24 @@ end
 
 
     end
-   
-    always@(*) begin
-       $monitor("Time: %t, startSignal:%d \n ", $time,startSignal);
-    end 
-
-
-   
-
-    always@(uut.Feature_Memory[31])
+   always@(uut.Feature_Memory[31])
     # 1500
     begin 
      $display(" Matrix Multiplication Output fetched from Feature Memory = \n %d  %d  %d  %d \n %d  %d  %d  %d \n %d  %d  %d  %d \n %d  %d  %d  %d \n  ",  uut.Feature_Memory[16],uut.Feature_Memory[17],uut.Feature_Memory[18],uut.Feature_Memory[19],uut.Feature_Memory[20],uut.Feature_Memory[21],uut.Feature_Memory[22],uut.Feature_Memory[23],uut.Feature_Memory[24], uut.Feature_Memory[25],uut.Feature_Memory[26],uut.Feature_Memory[27],uut.Feature_Memory[28],uut.Feature_Memory[29],uut.Feature_Memory[30],uut.Feature_Memory[31] );
     end
-
-
+    always@(*) begin
+       $monitor("Time: %t, startSignal:%d \n ", $time,startSignal);
+    end 
 
 endmodule
+   
+
+    /*always@(uut.Feature_Memory[31])
+    # 1500
+    begin 
+     $display(" Matrix Multiplication Output fetched from Feature Memory = \n %d  %d  %d  %d \n %d  %d  %d  %d \n %d  %d  %d  %d \n %d  %d  %d  %d \n  ",  uut.Feature_Memory[16],uut.Feature_Memory[17],uut.Feature_Memory[18],uut.Feature_Memory[19],uut.Feature_Memory[20],uut.Feature_Memory[21],uut.Feature_Memory[22],uut.Feature_Memory[23],uut.Feature_Memory[24], uut.Feature_Memory[25],uut.Feature_Memory[26],uut.Feature_Memory[27],uut.Feature_Memory[28],uut.Feature_Memory[29],uut.Feature_Memory[30],uut.Feature_Memory[31] );
+    end*/
+
+
 
 
