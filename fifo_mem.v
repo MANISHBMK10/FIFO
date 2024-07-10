d6f86f64b7f41ea624e22204daaeeae36d3329d4
Bmk10 <56593623+MANISHBMK10@users.noreply.github.com>
2024-07-10 19:15:01 -0400

    update fifo_mem

--- fifo_mem.v
+++ fifo_mem.v
@@ -1,6 +1,6 @@
 module fifomem #(parameter DATASIZE = 8, // Memory data word width
                  parameter ADDRSIZE = 4) // Number of mem address bits
-    (output reg [DATASIZE-1:0] rdata,
+    (output [DATASIZE-1:0] rdata,
      input [DATASIZE-1:0] wdata,
      input [ADDRSIZE-1:0] waddr, raddr,
      input wclken, wfull, wclk,rclk, rempty, rclken);
@@ -8,15 +8,15 @@ module fifomem #(parameter DATASIZE = 8, // Memory data word width
  // RTL Verilog memory model
      localparam DEPTH = 1<<ADDRSIZE;
      reg [DATASIZE-1:0] mem [0:DEPTH-1];
-    reg rdata_val;
-   //assign rdata = rdata_val;
+   // reg rdata_val;
+   assign rdata = mem[raddr];
     
 	always @(posedge wclk)
          if (wclken && !wfull)
      		 mem[waddr] <= wdata;
 
-     always @(posedge rclk)
-      if(rclken && !rempty)
-        rdata <= mem[raddr]; 
+     //always @(posedge rclk)
+      //if(rclken && !rempty)
+       // rdata <= mem[raddr]; 
  
 endmodule
