24c8486d326e80cb7b19c5420315cb5caacf79d6
Bmk10 <56593623+MANISHBMK10@users.noreply.github.com>
2024-08-01 22:01:28 -0400

    Update fifo_mem.v

--- fifo_mem.v
+++ fifo_mem.v
@@ -1,6 +1,6 @@
 module fifomem #(parameter DATASIZE = 8, // Memory data word width
                  parameter ADDRSIZE = 4) // Number of mem address bits
-    (output [DATASIZE-1:0] rdata,
+    (output reg [DATASIZE-1:0] rdata,
      input [DATASIZE-1:0] wdata,
      input [ADDRSIZE-1:0] waddr, raddr,
      input wclken, wfull, wclk,rclk, rempty, rclken);
@@ -9,14 +9,14 @@ module fifomem #(parameter DATASIZE = 8, // Memory data word width
      localparam DEPTH = 1<<ADDRSIZE;
      reg [DATASIZE-1:0] mem [0:DEPTH-1];
    // reg rdata_val;
-   assign rdata = mem[raddr];
+   //assign rdata = mem[raddr];
     
 	always @(posedge wclk)
          if (wclken && !wfull)
      		 mem[waddr] <= wdata;
 
-     //always @(posedge rclk)
-      //if(rclken && !rempty)
-       // rdata <= mem[raddr]; 
+     always @(posedge rclk)
+     if(rclken && !rempty)
+       rdata <= mem[raddr]; 
  
 endmodule
